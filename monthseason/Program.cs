9527da34c9fbe934236808159045baef6575ecfa
s2018 <s2018s35@e-mirim.hs.kr>
2020-05-06 15:18:08 +0900

    초기 커밋.

---
+++ monthseason/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace monthseason
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int month = int.Parse(Console.ReadLine());
+
+
+                switch (month)
+                {
+                    case 1: case 2: case 12: Console.WriteLine("winter"); break;
+                    case 3: case 4: case 5: Console.WriteLine("spring"); break;
+                    case 6: case 7: case 8: Console.WriteLine("summer"); break;
+                    case 9: case 10: case 11: Console.WriteLine("fall"); break;
+                default:
+                    Console.WriteLine("잘못 입력하셨습니다");
+                    break;
+                }
+            }
+        }
+    }
+
